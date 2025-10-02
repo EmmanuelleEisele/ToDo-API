e642b137420c27857df334a699e3e90d5a2e3f86
Emmanuelle Camarasa <manuecam2b@hotmail.fr>
2025-10-02 16:27:19 +0200

    auth middleware for protected routes

--- src/routers/taskRouter.js
+++ src/routers/taskRouter.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { taskController } from "../controllers/taskController.js";
+import { auth } from "../middlewares/auth.js";
 
 const router = Router();
 
 // Routes protégées
-router.get("/", taskController.getTasks);
-router.post("/", taskController.createTask);
-router.put("/:id", taskController.updateTask);
-router.delete("/:id", taskController.deleteTask);
+router.get("/", auth, taskController.getTasks);
+router.post("/", auth, taskController.createTask);
+router.put("/:id", auth, taskController.updateTask);
+router.delete("/:id", auth, taskController.deleteTask);
 
 export default router;
