3b451a28b9e029de5a2585a97019ab7f35d14437
Emmanuelle Camarasa <manuecam2b@hotmail.fr>
2025-11-19 15:57:58 +0100

    fix cors origin to let front & local acces

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use(cors({
     'http://127.0.0.1:5173',
     'http://localhost:5174',
     'http://127.0.0.1:5174',
-    'https://todo-front-yswc.onrender.com'
+    'https://todoom-green.vercel.app'
   ],
   credentials: true, 
 }));
